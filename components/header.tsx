e58d89f2e62dc67d539c3360853dc23c61037220
Chamesss <chamsedin.azouz@gmail.com>
2024-05-12 01:08:46 +0100

    minor fix

--- components/header.tsx
+++ components/header.tsx
@@ -29,7 +29,7 @@ export default function Header() {
                 className={clsx(
                   `flex w-full items-center justify-center p-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300`,
                   {
-                    "text-gray-950 dark:text-gray-200":
+                    "text-gray-950 dark:!text-gray-200":
                       activeSection === link.name,
                   }
                 )}

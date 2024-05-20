919b246dc01f2e5420f199b6cd2be08dd6e67553
ChamseDin Azouz <79670017+Chamesss@users.noreply.github.com>
2024-05-20 16:16:10 +0000

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
+import { Analytics } from '@vercel/analytics/react';
 import ActiveSectionContextProvider from "@/context/active-section-context";
 import { Toaster } from "react-hot-toast";
 import Footer from "@/components/footer";
@@ -50,6 +51,7 @@ export default function RootLayout({
           </ActiveSectionContextProvider>
           <ThemeSwitch />
         </ThemeContextProvider>
+        <Analytics />
       </body>
     </html>
   );

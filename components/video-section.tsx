1138678d491f5c9244cb198e43a86ecd7e9d1acc
Chamesss <chamsedin.azouz@gmail.com>
2024-11-22 15:35:26 +0100

    fix video overload

--- components/video-section.tsx
+++ components/video-section.tsx
@@ -10,7 +10,15 @@ export default function VideoSection({ className = '' }: { className?: string })
       <p className="mb-8">
         Here’s a glimpse of the many web components I’ve crafted—have a quick glance!
       </p>
-      <VideoPlayer src="/web-components.mp4" />
+      {/* <iframe
+        title="vimeo-player"
+        src="https://player.vimeo.com/video/1032355275?h=850126fdaa"
+        width="640"
+        height="360"
+        frameBorder={0}
+              allowFullScreen
+      ></iframe> */}
+      <VideoPlayer src="https://player.vimeo.com/video/1032355275?h=850126fdaa" />
     </div>
   )
 }
